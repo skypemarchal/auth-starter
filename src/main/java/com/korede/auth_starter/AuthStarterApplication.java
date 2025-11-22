2e6ca12b946711bde9f564a012a5699dbe8bdf45
korede <jacquesajmo@gmail.com>
2025-11-22 15:40:24 +0100

    init

---
+++ src/main/java/com/korede/auth_starter/AuthStarterApplication.java
@@ -0,0 +1,13 @@
+package com.korede.auth_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthStarterApplication.class, args);
+	}
+
+}
